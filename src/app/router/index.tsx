069f57a16dcb647b1217fabeeec409f7f40d9655
s._berdichevskaya <baiipstomsk20@gmail.com>
2026-04-08 21:20:52 +0700

    fix

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
 import AccountPage from "@/pages/account";

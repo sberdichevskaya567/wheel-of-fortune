0be8a10097d960fdb4b664db2bc2e1a14adfdbe1
s._berdichevskaya <baiipstomsk20@gmail.com>
2026-04-08 21:20:22 +0700

    fix

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,29 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
 import AccountPage from "@/pages/account";
 import MainLayout from "@/shared/components/layout/main-layout";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="/account" replace />,
-        },
-        {
-          path: "account",
-          element: <AccountPage />,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/wheel-of-fortune/",
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/account" replace />,
+      },
+      {
+        path: "account",
+        element: <AccountPage />,
+      },
+    ],
   },
-);
+]);
 
 export { router };
